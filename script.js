b8b089dcae0f6b8ef30de3a43b8e838b820a3851
tiajon1585 <tiajon1585@students.ecpi.edu>
2026-07-04 18:25:43 -0400

    Implement resource visibility based on project count
    Add functionality to display resources based on project count

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ var message = document.createElement("h2");
 message.textContent = "Welcome, " + userName + "!";
 
 document.body.insertBefore(message, document.body.firstChild);
+
+window.onload = function () {
+
+    let projectCount = 3;
+
+    let universityResources = document.getElementById("universityResources");
+    let personalProjects = document.getElementById("personalProjects");
+
+    if (projectCount < 3) {
+        universityResources.style.display = "block";
+        personalProjects.style.display = "block";
+    } else {
+        universityResources.style.display = "none";
+        personalProjects.style.display = "block";
+    }
+
+};
