4d06aacd9b9c8cf1ca936e2ec91d00dc692a3c3f
tiajon1585 <tiajon1585@students.ecpi.edu>
2026-07-05 13:06:28 -0400

    Fix duplicate window.onload function
    Removed redundant window.onload assignment.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+window.onload = function () {
 var userName = prompt("What is your name?");
 var message = document.createElement("h2");
 message.textContent = "Welcome, " + userName + "!";
 
 document.body.insertBefore(message, document.body.firstChild);
-
-window.onload = function () {
 
     // Featured content
     let projectCount = 3;
