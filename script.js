a05ae902db5c1f2873d67f579113b502019848c9
tiajon1585 <tiajon1585@students.ecpi.edu>
2026-07-20 14:25:45 -0400

    Update projectCount from 2 to 3

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ message.textContent = "Welcome, " + userName + "!";
 
 document.body.insertBefore(message, document.body.firstChild);
 
-    let projectCount = 2;
+    let projectCount = 3;
 
     let universityResources = document.getElementById("universityResources");
     let personalProjects = document.getElementById("personalProjects");
