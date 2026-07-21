da0afce495c577186ae99146e85f922a2862a1ad
tiajon1585 <tiajon1585@students.ecpi.edu>
2026-07-21 15:04:43 -0400

    Remove name prompt from window.onload function
    Removed user prompt for name input in window.onload function.

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-window.onload = function () {
-var userName = prompt("What is your name?");
-var message = document.createElement("h2");
-message.textContent = "Welcome, " + userName + "!";
-
-document.body.insertBefore(message, document.body.firstChild);
-
+window.onload = function ()
+// I removed the prompt() that was here
     let projectCount = 3;
 
     let universityResources = document.getElementById("universityResources");
